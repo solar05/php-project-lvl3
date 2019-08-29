245c24797b46425309f413fd40f61c53e1a599e8
solar05 <artem00298@gmail.com>
2019-08-29 15:50:27 +0300

    Add debugbar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -47,6 +47,8 @@ $app->singleton(
 );
 
 if (env('APP_DEBUG')) {
+    $app->configure('app');
+    $app->configure('debugbar');
     $app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
 }
 
