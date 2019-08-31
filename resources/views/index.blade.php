bc94669010c16b0d2f4a089e0f1f0bcd9f908da4
solar05 <artem00298@gmail.com>
2019-08-31 16:03:36 +0300

    Add layouting

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,30 +1,9 @@
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<html>
-    <body>
-    <nav class="navbar navbar-dark bg-dark">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Navigation</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </nav>
+@extends('layouts.app')
+@section('title', 'Page analyzer')
+@section('navbar')
+    @parent
+@endsection
+@section('content')
     <div class="jumbotron">
         <h1 class="display-4" align="center">Page Analyzer</h1>
         <p class="lead" align="center">Web-application to analyze sites for SEO suitability. Please input site URL that you want to analyze.</p>
@@ -36,5 +15,4 @@
             </form>
         </p>
     </div>
-    </body>
-</html>
+@endsection
