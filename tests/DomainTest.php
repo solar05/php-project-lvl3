bd8465accae162a0d28da541f5c6ef0fce128d5d
solar05 <artem00298@gmail.com>
2019-09-10 14:06:25 +0300

    Add testing

--- tests/DomainTest.php
+++ tests/DomainTest.php
@@ -3,27 +3,61 @@
 namespace Tests;
 
 use Laravel\Lumen\Testing\DatabaseMigrations;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+
+
 
 class DomainTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function testMainPage()
+
+    protected function setUp():void
+    {
+        parent::setUp();
+        $path = 'tests/fixtures/simple.html';
+        $body = file_get_contents($path);
+        $mock = new MockHandler([
+            new Response(200, ['Content-Length' => 9], $body)
+        ]);
+        $handler = HandlerStack::create($mock);
+        $this->app->bind('GuzzleHttp\Client', function ($app) use ($handler) {
+            return new Client(['handler' => $handler]);
+        });
+    }
+
+    public function testHomePage()
     {
         $response = $this->get(route('home'));
         $response->assertResponseStatus(200);
     }
 
-    public function testDomainAdding()
+    public function testDomainsStore()
     {
-        $response = $this->post(route('domains.store'), ['domain' => 'https://www.example.com']);
-        $response->assertResponseStatus(302);
-        $this->seeInDatabase('domains', ['name' => 'https://www.example.com']);
-        $response = $this->get(route('domains.show', ['id' => 1]));
-        $response->assertResponseStatus(200);
+        $domain = factory('App\Domain')->make();
+        $this->post(route('domains.store'), ['domain' => $domain->name]);
+        $this->assertResponseStatus(302);
+        $this->seeInDatabase('domains', [
+            'name' => $domain->name,
+            'status' => 200,
+            'content_length' => 9,
+            'header' => 'Hi header',
+            'content' => 'Another test in the wall'
+        ]);
     }
 
-    public function testShowDomains()
+    public function testDomainsShow()
+    {
+        $domain = factory('App\Domain')->make(['state' => 'initialized']);
+        $domain->save();
+        $this->get(route('domains.show', ['id' => $domain->id]));
+        $this->assertResponseStatus(200);
+    }
+
+    public function testDomainsIndex()
     {
         $response = $this->get(route('domains.index'));
         $response->assertResponseStatus(200);
